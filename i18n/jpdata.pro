3bbd65c50101474be8a649ec151547ad959abb6c
babayoshihiko <babayoshihiko@mac.com>
2024-05-08 06:55:07 +0900

    Added ja translation

---
+++ i18n/jpdata.pro
@@ -0,0 +1,4 @@
+FORMS = ../jpdata_dialog_base.ui
+SOURCES = ../jpdata_dialog.py ../jpdata.py ../jpDataUtils.py
+TRANSLATIONS = jpdata_ja.ts
+
---
+++ i18n/jpdata_ja.ts
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>jpdata</name>
+    <message>
+        <location filename="../jpdata.py" line="251"/>
+        <source>&amp;jpdata</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="238"/>
+        <source>Add Japan Data</source>
+        <translation type="unfinished">日本のデータを追加</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="268"/>
+        <source>Choose folder for zip/shp files</source>
+        <translation type="unfinished">フォルダを選択</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="270"/>
+        <source>LandNumInfo</source>
+        <translation type="unfinished">国土数値情報</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="271"/>
+        <source>GSI Tiles</source>
+        <translation type="unfinished">地理院タイル</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="274"/>
+        <source>Download</source>
+        <translation type="unfinished">ダウンロード</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="454"/>
+        <source>Choose Folder</source>
+        <translation type="unfinished">フォルダを選択</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="279"/>
+        <source>Unzip</source>
+        <translation type="unfinished">解凍</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="280"/>
+        <source>Unzip downloaded zipfile</source>
+        <translation type="unfinished">ダウンロードした zip ファイルを解凍</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="285"/>
+        <source>Add to Map</source>
+        <translation type="unfinished">地図に追加</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="283"/>
+        <source>Add Shapefile as a Layer to Map on QGIS</source>
+        <translation type="unfinished">QGIS の地図にシェープファイルをレイヤとして追加</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="286"/>
+        <source>Add GSI xyz tile server to Map on QGIS</source>
+        <translation type="unfinished">QGIS の地図に地理院 xyz タイルサーバを追加</translation>
+    </message>
+</context>
+<context>
+    <name>jpdataDialogBase</name>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="14"/>
+        <source>jpdata</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="39"/>
+        <source>myPushuButton2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="46"/>
+        <source>myLabel1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="57"/>
+        <source>Tab 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="92"/>
+        <source>myPushuButton1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="99"/>
+        <source>myPushuButton3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="106"/>
+        <source>myPushuButton4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="114"/>
+        <source>Tab 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="123"/>
+        <source>myPushButton5</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
