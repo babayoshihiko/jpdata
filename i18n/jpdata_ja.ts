a5cc051c7299264414fe0e8f5cc289ea5872d7cf
BABA Yoshihiko <babayoshihiko@mac.com>
2024-05-14 13:53:39 +0900

    Translated census

--- i18n/jpdata_ja.ts
+++ i18n/jpdata_ja.ts
@@ -4,64 +4,79 @@
 <context>
     <name>jpdata</name>
     <message>
-        <location filename="../jpdata.py" line="251"/>
+        <location filename="../jpdata.py" line="208"/>
         <source>&amp;jpdata</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="238"/>
+        <location filename="../jpdata.py" line="195"/>
         <source>Add Japan Data</source>
         <translation type="unfinished">日本のデータを追加</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="268"/>
+        <location filename="../jpdata.py" line="225"/>
         <source>Choose folder for zip/shp files</source>
         <translation type="unfinished">フォルダを選択</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="270"/>
+        <location filename="../jpdata.py" line="227"/>
         <source>LandNumInfo</source>
         <translation type="unfinished">国土数値情報</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="271"/>
+        <location filename="../jpdata.py" line="228"/>
         <source>GSI Tiles</source>
         <translation type="unfinished">地理院タイル</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="274"/>
+        <location filename="../jpdata.py" line="270"/>
         <source>Download</source>
         <translation type="unfinished">ダウンロード</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="454"/>
+        <location filename="../jpdata.py" line="525"/>
         <source>Choose Folder</source>
         <translation type="unfinished">フォルダを選択</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="279"/>
+        <location filename="../jpdata.py" line="236"/>
         <source>Unzip</source>
         <translation type="unfinished">解凍</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="280"/>
+        <location filename="../jpdata.py" line="237"/>
         <source>Unzip downloaded zipfile</source>
         <translation type="unfinished">ダウンロードした zip ファイルを解凍</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="285"/>
+        <location filename="../jpdata.py" line="273"/>
         <source>Add to Map</source>
         <translation type="unfinished">地図に追加</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="283"/>
+        <location filename="../jpdata.py" line="274"/>
         <source>Add Shapefile as a Layer to Map on QGIS</source>
         <translation type="unfinished">QGIS の地図にシェープファイルをレイヤとして追加</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="286"/>
+        <location filename="../jpdata.py" line="243"/>
         <source>Add GSI xyz tile server to Map on QGIS</source>
         <translation type="unfinished">QGIS の地図に地理院 xyz タイルサーバを追加</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="231"/>
+        <source>Download Land Numerical Information data</source>
+        <translation type="unfinished">国土数値情報を追加</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="268"/>
+        <source>Census</source>
+        <translation type="unfinished">国勢調査</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="271"/>
+        <source>Download census data by city</source>
+        <translation type="unfinished">市町村ごとに国勢調査を追加</translation>
     </message>
 </context>
 <context>
@@ -83,7 +98,7 @@
     </message>
     <message>
         <location filename="../jpdata_dialog_base.ui" line="57"/>
-        <source>Tab 1</source>
+        <source>tab_1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -103,12 +118,27 @@
     </message>
     <message>
         <location filename="../jpdata_dialog_base.ui" line="114"/>
-        <source>Tab 2</source>
+        <source>tab_2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../jpdata_dialog_base.ui" line="123"/>
         <source>myPushButton5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="131"/>
+        <source>tab_3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="140"/>
+        <source>myPushButton31</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dialog_base.ui" line="153"/>
+        <source>myPushButton32</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
