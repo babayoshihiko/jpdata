65d3838d2a950ad40aed8885835911dceb2c4d10
babayoshihiko <babayoshihiko@mac.com>
2024-09-08 06:04:16 +0900

    translation

--- i18n/jpdata_ja.ts
+++ i18n/jpdata_ja.ts
@@ -4,139 +4,154 @@
 <context>
     <name>jpdata</name>
     <message>
-        <location filename="../jpdata.py" line="220"/>
+        <location filename="../jpdata.py" line="223"/>
         <source>&amp;jpdata</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="261"/>
+        <location filename="../jpdata.py" line="331"/>
         <source>Choose folder for zip/shp files</source>
         <translation>フォルダを選択</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="270"/>
+        <location filename="../jpdata.py" line="264"/>
         <source>LandNumInfo</source>
         <translation>国土数値情報</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="300"/>
+        <location filename="../jpdata.py" line="294"/>
         <source>GSI Tiles</source>
         <translation>地理院タイル</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="647"/>
+        <location filename="../jpdata.py" line="662"/>
         <source>Download</source>
         <translation>ダウンロード</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="707"/>
+        <location filename="../jpdata.py" line="722"/>
         <source>Choose Folder</source>
         <translation>フォルダを選択</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="318"/>
+        <location filename="../jpdata.py" line="312"/>
         <source>Add to Map</source>
         <translation>地図に追加</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="319"/>
+        <location filename="../jpdata.py" line="313"/>
         <source>Add Shapefile as a Layer to Map on QGIS</source>
         <translation>QGIS の地図にシェープファイルをレイヤとして追加</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="302"/>
+        <location filename="../jpdata.py" line="296"/>
         <source>Add GSI xyz tile server to Map on QGIS</source>
         <translation>QGIS の地図に地理院 xyz タイルサーバを追加</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="272"/>
+        <location filename="../jpdata.py" line="266"/>
         <source>Download Land Numerical Information data</source>
         <translation>国土数値情報を追加</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="308"/>
+        <location filename="../jpdata.py" line="302"/>
         <source>Census</source>
         <translation>国勢調査</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="316"/>
+        <location filename="../jpdata.py" line="310"/>
         <source>Download census data by city</source>
         <translation>市町村ごとに国勢調査を追加</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="309"/>
+        <location filename="../jpdata.py" line="303"/>
         <source>Year</source>
         <translation>年</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="683"/>
+        <location filename="../jpdata.py" line="698"/>
         <source>Select a shp file</source>
         <translation>shp を選択</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="277"/>
+        <location filename="../jpdata.py" line="271"/>
         <source>Web</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="278"/>
+        <location filename="../jpdata.py" line="272"/>
         <source>Open the webpage with the standard browser</source>
         <translation>標準ブラウザでWebページを開く</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="662"/>
+        <location filename="../jpdata.py" line="677"/>
         <source>Cancel</source>
         <translation>キャンセル</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="554"/>
+        <location filename="../jpdata.py" line="569"/>
         <source>Cannot find the .shp file: </source>
         <translation>.shp ファイルを見つけられません: </translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="605"/>
+        <location filename="../jpdata.py" line="620"/>
         <source>Downloading: </source>
         <translation>ダウンロード: </translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="610"/>
+        <location filename="../jpdata.py" line="625"/>
         <source>The zip file exists: </source>
         <translation>zip ファイルが存在しています: </translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="618"/>
+        <location filename="../jpdata.py" line="633"/>
         <source>...Done</source>
         <translation>...終了</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="629"/>
+        <location filename="../jpdata.py" line="644"/>
         <source>...Cancelled</source>
         <translation>...キャンセル</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="392"/>
+        <location filename="../jpdata.py" line="407"/>
         <source>Nation-wide</source>
         <translation>全国</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="712"/>
+        <location filename="../jpdata.py" line="727"/>
         <source>The folder is not writable.</source>
         <translation>フォルダに書き込むことができません。</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="188"/>
+        <location filename="../jpdata.py" line="191"/>
         <source>jpdata</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="265"/>
+        <location filename="../jpdata.py" line="336"/>
         <source>User/Password are not stored</source>
         <translation>ユーザ名/パスワードは保存されません</translation>
     </message>
     <message>
-        <location filename="../jpdata.py" line="330"/>
+        <location filename="../jpdata.py" line="324"/>
         <source>Setting</source>
         <translation>設定</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="338"/>
+        <source>Test</source>
+        <translation type="unfinished">テスト</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="342"/>
+        <source>Prefecture</source>
+        <translation>都道府県</translation>
+    </message>
+    <message>
+        <location filename="../jpdata.py" line="343"/>
+        <source>Municipality</source>
+        <translation>（区）市町村</translation>
     </message>
 </context>
 <context>
@@ -147,12 +162,12 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="34"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="21"/>
         <source>myLabelStatus</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="45"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="32"/>
         <source>tab_1</source>
         <translation type="unfinished"></translation>
     </message>
@@ -172,68 +187,73 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="114"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="121"/>
         <source>tab_2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="123"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="130"/>
         <source>myPushButton25</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="131"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="138"/>
         <source>tab_3</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="148"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="155"/>
         <source>myLabel31</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="174"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="201"/>
         <source>myPushButton32</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="181"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="194"/>
         <source>myPushButton31</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="193"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="213"/>
         <source>tab_setting</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="206"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="247"/>
         <source>Proxy Server</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="220"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="233"/>
         <source>Shape Folder</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="229"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="283"/>
         <source>myLabel1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="242"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="296"/>
         <source>myPushuButton2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="251"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="226"/>
         <source>Proxy User</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../jpdata_dockwidget_base.ui" line="271"/>
+        <location filename="../jpdata_dockwidget_base.ui" line="254"/>
         <source>Proxy Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../jpdata_dockwidget_base.ui" line="305"/>
+        <source>myPushButtonTest</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
