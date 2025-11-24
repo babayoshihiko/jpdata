a1026b3f42fefda7afe64127570886b362e72fde
babayoshihiko <babayoshihiko@mac.com>
2025-11-25 07:15:48 +0900

    A31

--- helper_script/create_csv_A31.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eV
-
-
-curl https://nlftp.mlit.go.jp/ksj/gml/data/A31a/A31a-24/A31a-24_81_10_SHP.zip -o ~/data.noindex/A31/A31a-24_81_10_SHP.zip
-python3.10 helper_script/create_csv_A31.py 2024 81 北海道開発局
-
-curl https://nlftp.mlit.go.jp/ksj/gml/data/A31a/A31a-24/A31a-24_82_10_SHP.zip -o ~/data.noindex/A31/A31a-24_82_10_SHP.zip
-python3.10 helper_script/create_csv_A31.py 2024 82 東北地方整備局
