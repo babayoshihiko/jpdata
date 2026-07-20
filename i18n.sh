2eb2a7196fc46005a7c3ea9d95e0b79f3086f421
Yoshihiko Baba <babayoshihiko@mac.com>
2026-07-20 09:34:38 +0900

    development tools

--- i18n.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -eV
-
-pylupdate5 -noobsolete i18n/jpdata.pro
-
-# open .ts file with linguist 
-
-lrelease
-
---
+++ mac/i18n.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -eV
+
+./pylupdate5 -noobsolete ../i18n/jpdata.pro
+
+# open .ts file with linguist 
+
+# QGIS 3.40 has Designer.app in Contents/MacOS but not Linguist.app
